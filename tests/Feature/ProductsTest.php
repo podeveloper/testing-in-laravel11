362cf0836e45cf70825979e574ae7ddfc61fdedc
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2024-07-21 19:17:23 +0300

    AssertDontSee Example

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Product;
 use App\Models\User;
 
+use function Pest\Laravel\{get};
 use function Pest\Laravel\{actingAs};
 
 test('homepage contains empty table', function () {
@@ -11,4 +13,18 @@ test('homepage contains empty table', function () {
     actingAs($user)->get('/products')
         ->assertStatus(200)
         ->assertSee(__('No products found'));
+});
+
+
+test('homepage contains non empty table', function () {
+    Product::create([
+        'name'  => 'Product 1',
+        'price' => 123,
+    ]);
+
+    $user = User::factory()->create();
+
+    actingAs($user)->get('/products')
+        ->assertStatus(200)
+        ->assertDontSee(__('No products found'));
 });
